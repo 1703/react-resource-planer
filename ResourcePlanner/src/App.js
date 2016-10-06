0ebb5e40102bcba51138fe6492d430ccbb91fdb2
Wolfgang Reithmeier <w.reithmeier@gmail.com>
2016-10-06 09:57:10 +0200

    Person comp

--- ResourcePlanner/src/App.js
+++ ResourcePlanner/src/App.js
@@ -4,19 +4,29 @@ import './bootstrap/css/bootstrap.min.css'
 import './bootstrap/css/bootstrap-theme.min.css'
 import logo from './logo.svg';
 import './App.css';
-import Project from './components/project'
+import Person from './components/person'
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-        <Grid>
-          <Row>
-              <h2>Resource Planer</h2>
-          </Row>
-          <Row>
-              <Project />
-          </Row>
-        </Grid>
+        <MuiThemeProvider>
+            <Grid>
+              <Row>
+                  <h2>Resource Planer</h2>
+              </Row>
+              <Row>
+                  <Person name={"Testi"} />
+              </Row>
+            </Grid>
+        </MuiThemeProvider>
     );
   }
 }
--- ResourcePlanner/src/components/person.js
+++ ResourcePlanner/src/components/person.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react'
 
+import Avatar from 'material-ui/Avatar';
+
+import {
+    blue300,
+    indigo900,
+    orange200,
+    deepOrange300,
+    pink400,
+    purple500,
+    grey400
+} from 'material-ui/styles/colors';
+
 export default class Person extends Component {
+
+// --+id (prop)
+// --+name (prop)
+// --+projects:Projects (prop)
+// --+role (prop)
+
+
+    constructor (props) {
+        super (props)
+
+    }
 
     render () {
 
         var self = this;
 
-        return <div>
-            HOME :-)
-        </div>
+        return <Avatar
+            color={blue300}
+            backgroundColor={grey400}
+            size={50}
+        >{ self.props.name.substring(0,1)}</Avatar>
     }
 }
