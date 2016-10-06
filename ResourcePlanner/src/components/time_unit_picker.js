6d2179e379e6124898d2702ee4656309f70a171f
Administrator <admin@OTTDevBook.local>
2016-10-06 09:41:58 +0200

    added material ui, started unit picker

--- ResourcePlanner/src/components/time_unit_picker.js
+++ ResourcePlanner/src/components/time_unit_picker.js
@@ -1,13 +1,27 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {SplitButton, MenuItem} from 'react-bootstrap';
 
 export default class TimeUnitPicker extends Component {
-
+    constructor (props) {
+      super (props);
+    
+      this.state = {
+        inputValue: "",
+        data: props.data || ['Erster', 'Zweiter', 'Dritter']
+      }
+    
+      this.handleUnitSelected = this.handleUnitSelected.bind(this);
+    }
+  
     render () {
 
         var self = this;
 
-        return <div>
-            HOME :-)
-        </div>
+        return (
+          <SplitButton title="Time Unit Picker" pullRight id="time-unit-picker" onSelect={this.handleUnitSelected}>
+            <MenuItem eventKey="1">Wochen Ansicht</MenuItem>
+            <MenuItem eventKey="2">Monats Ansicht</MenuItem>
+          </SplitButton>
+        )
     }
 }
